7b8f1c7cc32808f74cbcdac03d0fc12f4dc6bd5c
nixpig <143995476+nixpig@users.noreply.github.com>
2024-05-25 07:27:12 +0100

    chore: add initial cobra framework

---
+++ cmd/root.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "syringe",
+	Short: "A terminal-based utility to securely manage environment variables across projects and environments.",
+	Long:  "A terminal-based utility to securely manage environment variables across projects and environments.",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.syringe.sh.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2024 https://github.com/nixpig
+*/
+package main
+
+import "github.com/nixpig/syringe.sh/cmd"
+
+func main() {
+	cmd.Execute()
+}
