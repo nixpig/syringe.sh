54990c629772e0f254b1e81cec81f18efc9213c8
nixpig <143995476+nixpig@users.noreply.github.com>
2024-06-22 14:33:46 +0100

    chore: ongoing work on ssh auth

--- pkg/ssh/client.go
+++ pkg/ssh/client.go
@@ -1,12 +1,17 @@
 package client
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"os/user"
+	"path/filepath"
 
 	gossh "golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+	"golang.org/x/crypto/ssh/knownhosts"
 	"golang.org/x/term"
 )
 
@@ -16,49 +21,117 @@ func SSHClient(
 	identity string,
 	command string,
 ) ([]byte, error) {
-	keyFile, err := os.Open(identity)
-	if err != nil {
+	currentUser, err := user.Current()
+	if err != nil || currentUser.Username == "" {
 		return nil, err
 	}
 
-	keyContents, err := io.ReadAll(keyFile)
-	if err != nil {
-		return nil, err
+	sshConfig := &gossh.ClientConfig{
+		User: currentUser.Username,
 	}
 
-	var signer gossh.Signer
+	sshAuthSock := os.Getenv("SSH_AUTH_SOCK")
 
-	signer, err = gossh.ParsePrivateKey(keyContents)
-	if err != nil {
-		_, ok := err.(*gossh.PassphraseMissingError)
-		if !ok {
-			return nil, err
-		}
+	// if sshAuthSock == "" {
+	if identity == "" {
+		var signer gossh.Signer
 
-		fmt.Printf("Enter passphrase for key '%s': ", identity)
-		passphrase, err := term.ReadPassword(int(os.Stdin.Fd()))
-		fmt.Print("\n")
+		keyFile, err := os.Open(identity)
 		if err != nil {
 			return nil, err
 		}
 
-		signer, err = gossh.ParsePrivateKeyWithPassphrase(keyContents, passphrase)
+		keyContents, err := io.ReadAll(keyFile)
 		if err != nil {
 			return nil, err
 		}
-	}
 
-	config := &gossh.ClientConfig{
-		User: "nixpig",
-		Auth: []gossh.AuthMethod{
+		signer, err = gossh.ParsePrivateKey(keyContents)
+		if err != nil {
+			_, ok := err.(*gossh.PassphraseMissingError)
+			if !ok {
+				return nil, err
+			}
+
+			fmt.Printf("Enter passphrase for key '%s': ", identity)
+			passphrase, err := term.ReadPassword(int(os.Stdin.Fd()))
+			fmt.Print("\n")
+			if err != nil {
+				return nil, err
+			}
+
+			signer, err = gossh.ParsePrivateKeyWithPassphrase(keyContents, passphrase)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		sshConfig.Auth = []gossh.AuthMethod{
 			gossh.PublicKeys(signer),
-		},
-		HostKeyCallback: gossh.HostKeyCallback(func(hostname string, remote net.Addr, key gossh.PublicKey) error {
+		}
+
+		sshConfig.HostKeyCallback = gossh.HostKeyCallback(func(hostname string, remote net.Addr, key gossh.PublicKey) error {
+			fmt.Println("calling back...")
+			khf := filepath.Join(os.Getenv("HOME"), ".ssh", "known_hosts")
+
+			fmt.Println("creating knownhosts for: ", khf)
+			kh, err := knownhosts.New(khf)
+			if err != nil {
+				return errors.New("couldn't open known_hosts file")
+			}
+
+			var kerr *knownhosts.KeyError
+
+			fmt.Println("checking against knownhosts")
+			fmt.Println("host: ", host)
+			fmt.Println("remote: ", remote.String())
+			fmt.Println("key: ", key)
+			if err := kh(fmt.Sprintf("%s:%d", host, port), remote, key); err != nil {
+				fmt.Println("checking if matches want")
+				if errors.As(err, &kerr) && len(kerr.Want) > 0 {
+					return fmt.Errorf("host %s is not a key of %s: %w", key, host, err)
+				}
+
+				fmt.Println("checking if present")
+				if errors.As(err, &kerr) && len(kerr.Want) == 0 {
+					fmt.Println("host key not present; adding to known hosts")
+
+					khfh, err := os.OpenFile(khf, os.O_APPEND|os.O_WRONLY, 0600)
+					if err != nil {
+						return errors.New("unable to open known hosts file for writing")
+					}
+
+					defer khfh.Close()
+
+					knownhost := knownhosts.Normalize(remote.String())
+
+					fmt.Println("writing new known hosts")
+					if _, err := khfh.WriteString(knownhosts.Line([]string{knownhost}, key)); err != nil {
+						return errors.New("failed to write to knownhosts")
+					}
+				}
+			}
+
 			return nil
-		}),
+		})
+
+	} else {
+		sshAgent, err := net.Dial("unix", sshAuthSock)
+		if err != nil {
+			return nil, err
+		}
+
+		sshConfig.Auth = []gossh.AuthMethod{
+			gossh.PublicKeysCallback(agent.NewClient(sshAgent).Signers),
+		}
+
+		sshConfig.HostKeyCallback = gossh.HostKeyCallback(func(hostname string, remote net.Addr, key gossh.PublicKey) error {
+			// TODO: verify keys when used from agent
+			return nil
+		})
 	}
 
-	conn, err := gossh.Dial("tcp", fmt.Sprintf("%s:%d", host, port), config)
+	conn, err := gossh.Dial("tcp", fmt.Sprintf("%s:%d", host, port), sshConfig)
 	if err != nil {
 		return nil, err
 	}
