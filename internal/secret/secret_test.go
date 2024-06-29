3bb34ed64b207e7093cf9f42ce7945a9f37da9c3
nixpig <143995476+nixpig@users.noreply.github.com>
2024-06-29 10:04:20 +0100

    test: re-enable happy path tests for secret command

--- internal/secret/secret_test.go
+++ internal/secret/secret_test.go
@@ -1,135 +1,143 @@
 package secret_test
 
-// import (
-// 	"bytes"
-// 	"database/sql"
-// 	"errors"
-// 	"fmt"
-// 	"io"
-// 	"os"
-// 	"regexp"
-// 	"strings"
-// 	"testing"
-//
-// 	"github.com/DATA-DOG/go-sqlmock"
-// 	"github.com/nixpig/syringe.sh/cmd/server/servercmd"
-// 	"github.com/nixpig/syringe.sh/internal/secret"
-// 	"github.com/nixpig/syringe.sh/test"
-// 	"github.com/spf13/cobra"
-// 	"github.com/stretchr/testify/require"
-// )
-//
-// func TestSecretCmd(t *testing.T) {
-// 	scenarios := map[string]func(t *testing.T, mock sqlmock.Sqlmock, db *sql.DB){
-// 		"test secret set command happy path":          testSecretSetCmdHappyPath,
-// 		"test secret set command missing project":     testSecretSetCmdMissingProject,
-// 		"test secret set command missing environment": testSecretSetCmdMissingEnvironment,
-// 		"test secret set command too few args":        testSecretSetCmdTooFewArgs,
-// 		"test secret set command too many args":       testSecretSetCmdTooManyArgs,
-// 		"test secret set command database error":      testSecretSetCmdDatabaseError,
-// 		"test secret set command validation error":    testSecretSetCmdValidationError,
-//
-// 		"test secret get command happy path":          testSecretGetCmdHappyPath,
-// 		"test secret get command missing project":     testSecretGetCmdMissingProject,
-// 		"test secret get command missing environment": testSecretGetCmdMissingEnvironment,
-// 		"test secret get command missing key":         testSecretGetCmdMissingKey,
-// 		"test secret get command database error":      testSecretGetCmdDatabaseError,
-// 		"test secret get command validation error":    testSecretGetCmdValidationError,
-//
-// 		"test secret list command happy path":          testSecretListCmdHappyPath,
-// 		"test secret list command zero results":        testSecretListCmdZeroResults,
-// 		"test secret list command database error":      testSecretListCmdDatabaseError,
-// 		"test secret list command missing project":     testSecretListCmdMissingProject,
-// 		"test secret list command missing environment": testSecretListCmdMissingEnvironment,
-// 		"test secret list command validation error":    testSecretListCmdValidationError,
-//
-// 		"test secret remove command happy path":          testSecretRemoveCmdHappyPath,
-// 		"test secret remove command zero results":        testSecretRemoveCmdZeroResults,
-// 		"test secret remove command database error":      testSecretRemoveCmdDatabaseError,
-// 		"test secret remove command missing project":     testSecretRemoveCmdMissingProject,
-// 		"test secret remove command missing environment": testSecretRemoveCmdMissingEnvironment,
-// 		"test secret remove command missing key":         testSecretRemoveCmdMissingKey,
-// 		"test secret remove command validation error":    testSecretRemoveCmdValidationError,
-// 	}
-//
-// 	for scenario, fn := range scenarios {
-// 		t.Run(scenario, func(t *testing.T) {
-// 			db, mock, err := sqlmock.New()
-// 			if err != nil {
-// 				t.Fatalf("unable to create mock database:\n%s", err)
-// 			}
-//
-// 			fn(t, mock, db)
-// 		})
-// 	}
-// }
-//
-// func testSecretSetCmdHappyPath(t *testing.T, mock sqlmock.Sqlmock, db *sql.DB) {
-// 	cmdIn := bytes.NewReader([]byte{})
-// 	cmdOut := bytes.NewBufferString("")
-//
-// 	query := `
-// 		insert into secrets_
-// 		(key_, value_, environment_id_)
-// 		values (
-// 			$key,
-// 			$value,
-// 			(
-// 				select e.id_ from
-// 					environments_ e
-// 					inner join
-// 					projects_ p
-// 					on e.project_id_ = p.id_
-// 					where p.name_ = $project
-// 					and e.name_ = $environment
-// 			)
-// 		)
-// 	`
-//
-// 	mock.ExpectExec(regexp.QuoteMeta(query)).
-// 		WithArgs(
-// 			"my_cool_project",
-// 			"staging",
-// 			"secret_key",
-// 			"secret_value",
-// 		).
-// 		WillReturnResult(sqlmock.NewResult(1, 1))
-//
-// 	err := servercmd.Execute(
-// 		[]*cobra.Command{secret.SecretCommand()},
-// 		[]string{
-// 			"secret",
-// 			"set",
-// 			"-p",
-// 			"my_cool_project",
-// 			"-e",
-// 			"staging",
-// 			"secret_key",
-// 			"secret_value",
-// 		},
-// 		cmdIn,
-// 		cmdOut,
-// 		os.Stderr,
-// 		db,
-// 	)
-//
-// 	require.NoError(t, err)
-//
-// 	out, err := io.ReadAll(cmdOut)
-// 	if err != nil {
-// 		t.Errorf("failed to read from out")
-// 	}
-//
-// 	require.Equal(
-// 		t,
-// 		"",
-// 		string(out),
-// 		"should not output anything",
-// 	)
-//
-// 	require.NoError(t, mock.ExpectationsWereMet())
-// }
-//
+import (
+	"bytes"
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/nixpig/syringe.sh/internal/secret"
+	"github.com/nixpig/syringe.sh/pkg/validation"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSecretCmd(t *testing.T) {
+	scenarios := map[string]func(
+		t *testing.T,
+		cmd *cobra.Command,
+		service secret.SecretService,
+		mock sqlmock.Sqlmock,
+	){
+		"test secret set command happy path": testSecretSetCmdHappyPath,
+		// "test secret set command missing project":     testSecretSetCmdMissingProject,
+		// "test secret set command missing environment": testSecretSetCmdMissingEnvironment,
+		// "test secret set command too few args":        testSecretSetCmdTooFewArgs,
+		// "test secret set command too many args":       testSecretSetCmdTooManyArgs,
+		// "test secret set command database error":      testSecretSetCmdDatabaseError,
+		// "test secret set command validation error":    testSecretSetCmdValidationError,
+
+		"test secret get command happy path": testSecretGetCmdHappyPath,
+		// "test secret get command missing project":     testSecretGetCmdMissingProject,
+		// "test secret get command missing environment": testSecretGetCmdMissingEnvironment,
+		// "test secret get command missing key":         testSecretGetCmdMissingKey,
+		// "test secret get command database error":      testSecretGetCmdDatabaseError,
+		// "test secret get command validation error":    testSecretGetCmdValidationError,
+		//
+		"test secret list command happy path": testSecretListCmdHappyPath,
+		// "test secret list command zero results":        testSecretListCmdZeroResults,
+		// "test secret list command database error":      testSecretListCmdDatabaseError,
+		// "test secret list command missing project":     testSecretListCmdMissingProject,
+		// "test secret list command missing environment": testSecretListCmdMissingEnvironment,
+		// "test secret list command validation error":    testSecretListCmdValidationError,
+
+		"test secret remove command happy path": testSecretRemoveCmdHappyPath,
+		// "test secret remove command zero results":        testSecretRemoveCmdZeroResults,
+		// "test secret remove command database error":      testSecretRemoveCmdDatabaseError,
+		// "test secret remove command missing project":     testSecretRemoveCmdMissingProject,
+		// "test secret remove command missing environment": testSecretRemoveCmdMissingEnvironment,
+		// "test secret remove command missing key":         testSecretRemoveCmdMissingKey,
+		// "test secret remove command validation error":    testSecretRemoveCmdValidationError,
+	}
+
+	for scenario, fn := range scenarios {
+		t.Run(scenario, func(t *testing.T) {
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("unable to create mock database:\n%s", err)
+			}
+
+			service := secret.NewSecretServiceImpl(
+				secret.NewSqliteSecretStore(db),
+				validation.NewValidator(),
+			)
+
+			cmd := secret.NewCmdSecret()
+
+			fn(t, cmd, service, mock)
+		})
+	}
+}
+
+func testSecretSetCmdHappyPath(
+	t *testing.T,
+	cmd *cobra.Command,
+	service secret.SecretService,
+	mock sqlmock.Sqlmock,
+) {
+	cmdIn := bytes.NewReader([]byte{})
+	cmdOut := bytes.NewBufferString("")
+	errOut := bytes.NewBufferString("")
+
+	cmdSet := secret.NewCmdSecretSet(
+		secret.NewHandlerSecretSet(service),
+	)
+
+	cmd.AddCommand(cmdSet)
+	cmd.SetArgs([]string{
+		"set",
+		"-p",
+		"my_cool_project",
+		"-e",
+		"staging",
+		"secret_key",
+		"secret_value",
+	})
+	cmd.SetIn(cmdIn)
+	cmd.SetOut(cmdOut)
+	cmd.SetErr(errOut)
+
+	query := `
+		insert into secrets_
+		(key_, value_, environment_id_)
+		values (
+			$key,
+			$value,
+			(
+				select e.id_ from
+					environments_ e
+					inner join
+					projects_ p
+					on e.project_id_ = p.id_
+					where p.name_ = $project
+					and e.name_ = $environment
+			)
+		)
+	`
+
+	mock.ExpectExec(regexp.QuoteMeta(query)).
+		WithArgs(
+			"my_cool_project",
+			"staging",
+			"secret_key",
+			"secret_value",
+		).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err := cmd.Execute()
+
+	require.NoError(t, err)
+	require.Empty(t, errOut.String())
+
+	require.Equal(
+		t,
+		"", // maybe we want to print out like a 'secret added' message in future?
+		cmdOut.String(),
+	)
+
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
 // func testSecretSetCmdMissingProject(t *testing.T, mock sqlmock.Sqlmock, db *sql.DB) {
 // 	cmdIn := bytes.NewReader([]byte{})
 // 	cmdOut := bytes.NewBufferString("")
@@ -372,79 +380,82 @@ package secret_test
 // 		string(out),
 // 	)
 // }
-//
-// func testSecretGetCmdHappyPath(t *testing.T, mock sqlmock.Sqlmock, db *sql.DB) {
-// 	cmdIn := bytes.NewReader([]byte{})
-// 	cmdOut := bytes.NewBufferString("")
-//
-// 	query := `
-// 		select s.id_, s.key_, s.value_, p.name_, e.name_
-// 		from secrets_ s
-// 		inner join
-// 		environments_ e
-// 		on s.environment_id_ = e.id_
-// 		inner join
-// 		projects_ p
-// 		on p.id_ = e.project_id_
-// 		where p.name_ = $project
-// 		and e.name_ = $environment
-// 		and s.key_ = $key
-// 	`
-//
-// 	mock.ExpectQuery(regexp.QuoteMeta(query)).
-// 		WithArgs(
-// 			"my_cool_project",
-// 			"staging",
-// 			"secret_key",
-// 		).
-// 		WillReturnRows(mock.NewRows([]string{
-// 			"id_",
-// 			"key_",
-// 			"value_",
-// 			"project_name_",
-// 			"environment_name_",
-// 		}).AddRow(
-// 			23,
-// 			"secret_key",
-// 			"secret_value",
-// 			"my_cool_project",
-// 			"staging",
-// 		))
-//
-// 	err := servercmd.Execute(
-// 		[]*cobra.Command{secret.SecretCommand()},
-// 		[]string{
-// 			"secret",
-// 			"get",
-// 			"-p",
-// 			"my_cool_project",
-// 			"-e",
-// 			"staging",
-// 			"secret_key",
-// 		},
-// 		cmdIn,
-// 		cmdOut,
-// 		os.Stderr,
-// 		db,
-// 	)
-//
-// 	require.NoError(t, err)
-//
-// 	out, err := io.ReadAll(cmdOut)
-// 	if err != nil {
-// 		t.Errorf("failed to read from out")
-// 	}
-//
-// 	require.Equal(
-// 		t,
-// 		"secret_value",
-// 		string(out),
-// 		"should not output anything",
-// 	)
-//
-// 	require.NoError(t, mock.ExpectationsWereMet())
-// }
-//
+
+func testSecretGetCmdHappyPath(
+	t *testing.T,
+	cmd *cobra.Command,
+	service secret.SecretService,
+	mock sqlmock.Sqlmock,
+) {
+	cmdIn := bytes.NewReader([]byte{})
+	cmdOut := bytes.NewBufferString("")
+	errOut := bytes.NewBufferString("")
+
+	cmdGet := secret.NewCmdSecretGet(
+		secret.NewHandlerSecretGet(service),
+	)
+
+	cmd.AddCommand(cmdGet)
+	cmd.SetArgs([]string{
+		"get",
+		"-p",
+		"my_cool_project",
+		"-e",
+		"staging",
+		"secret_key",
+	})
+	cmd.SetIn(cmdIn)
+	cmd.SetOut(cmdOut)
+	cmd.SetErr(errOut)
+
+	query := `
+		select s.id_, s.key_, s.value_, p.name_, e.name_
+		from secrets_ s
+		inner join
+		environments_ e
+		on s.environment_id_ = e.id_
+		inner join
+		projects_ p
+		on p.id_ = e.project_id_
+		where p.name_ = $project
+		and e.name_ = $environment
+		and s.key_ = $key
+	`
+
+	mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(
+			"my_cool_project",
+			"staging",
+			"secret_key",
+		).
+		WillReturnRows(mock.NewRows([]string{
+			"id_",
+			"key_",
+			"value_",
+			"project_name_",
+			"environment_name_",
+		}).AddRow(
+			23,
+			"secret_key",
+			"secret_value",
+			"my_cool_project",
+			"staging",
+		))
+
+	err := cmd.Execute()
+
+	require.NoError(t, err)
+	require.Empty(t, errOut.String())
+
+	require.Equal(
+		t,
+		"secret_value",
+		cmdOut.String(),
+	)
+
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
 // func testSecretGetCmdMissingProject(t *testing.T, mock sqlmock.Sqlmock, db *sql.DB) {
 // 	cmdIn := bytes.NewReader([]byte{})
 // 	cmdOut := bytes.NewBufferString("")
@@ -632,115 +643,136 @@ package secret_test
 // 		string(out),
 // 	)
 // }
-//
-// func testSecretListCmdHappyPath(t *testing.T, mock sqlmock.Sqlmock, db *sql.DB) {
-// 	cmdIn := bytes.NewReader([]byte{})
-// 	cmdOut := bytes.NewBufferString("")
-// 	errOut := bytes.NewBufferString("")
-//
-// 	query := `
-// 		select s.id_, s.key_, s.value_, p.name_, e.name_
-// 		from secrets_ s
-// 		inner join
-// 		environments_ e
-// 		on s.environment_id_ = e.id_
-// 		inner join
-// 		projects_ p
-// 		on p.id_ = e.project_id_
-// 		where p.name_ = $project
-// 		and e.name_ = $environment
-// 	`
-//
-// 	mock.
-// 		ExpectQuery(regexp.QuoteMeta(query)).
-// 		WithArgs("my_cool_project", "staging").
-// 		WillReturnRows(
-// 			sqlmock.NewRows([]string{
-// 				"id_",
-// 				"key_",
-// 				"value_",
-// 				"project_name_",
-// 				"environment_name_",
-// 			}).
-// 				AddRow(1, "key_1", "value_1", "my_cool_project", "staging").
-// 				AddRow(2, "key_2", "value_2", "my_cool_project", "staging"),
-// 		)
-//
-// 	err := servercmd.Execute(
-// 		[]*cobra.Command{secret.SecretCommand()},
-// 		[]string{"secret", "list", "-p", "my_cool_project", "-e", "staging"},
-// 		cmdIn,
-// 		cmdOut,
-// 		errOut,
-// 		db,
-// 	)
-//
-// 	require.NoError(t, err)
-//
-// 	out, err := io.ReadAll(cmdOut)
-// 	if err != nil {
-// 		t.Errorf("unable to read from cmd out")
-// 	}
-//
-// 	require.Equal(t, "key_1=value_1\nkey_2=value_2", string(out))
-//
-// 	require.NoError(t, mock.ExpectationsWereMet())
-// }
-//
-// func testSecretRemoveCmdHappyPath(t *testing.T, mock sqlmock.Sqlmock, db *sql.DB) {
-// 	cmdIn := bytes.NewReader([]byte{})
-// 	cmdOut := bytes.NewBufferString("")
-// 	errOut := bytes.NewBufferString("")
-//
-// 	query := `
-// 		delete from secrets_
-// 		where id_ in (
-// 			select s.id_ from secrets_ s
-// 			inner join
-// 			environments_ e
-// 			on s.environment_id_ = e.id_
-// 			inner join
-// 			projects_ p
-// 			on e.project_id_ = p.id_
-// 			where p.name_ = $projectName
-// 			and e.name_ = $environmentName
-// 			and s.key_ = $key
-// 		)
-// 	`
-//
-// 	mock.ExpectExec(regexp.QuoteMeta(query)).WithArgs(
-// 		"my_cool_project", "staging", "key_1",
-// 	).WillReturnResult(sqlmock.NewResult(23, 1))
-//
-// 	err := servercmd.Execute(
-// 		[]*cobra.Command{secret.SecretCommand()},
-// 		[]string{
-// 			"secret",
-// 			"remove",
-// 			"-p",
-// 			"my_cool_project",
-// 			"-e",
-// 			"staging",
-// 			"key_1",
-// 		},
-// 		cmdIn,
-// 		cmdOut,
-// 		errOut,
-// 		db,
-// 	)
-//
-// 	require.NoError(t, err)
-//
-// 	out, err := io.ReadAll(cmdOut)
-// 	if err != nil {
-// 		t.Error("unable to read from cmd out")
-// 	}
-//
-// 	require.Equal(t, string(out), "", "shouldn't output anything")
-//
-// 	require.NoError(t, mock.ExpectationsWereMet())
-// }
-//
+
+func testSecretListCmdHappyPath(
+	t *testing.T,
+	cmd *cobra.Command,
+	service secret.SecretService,
+	mock sqlmock.Sqlmock,
+) {
+	cmdIn := bytes.NewReader([]byte{})
+	cmdOut := bytes.NewBufferString("")
+	errOut := bytes.NewBufferString("")
+
+	cmdList := secret.NewCmdSecretList(
+		secret.NewHandlerSecretList(service),
+	)
+
+	cmd.AddCommand(cmdList)
+	cmd.SetArgs([]string{
+		"list",
+		"-p",
+		"my_cool_project",
+		"-e",
+		"staging",
+	})
+	cmd.SetIn(cmdIn)
+	cmd.SetOut(cmdOut)
+	cmd.SetErr(errOut)
+
+	query := `
+		select s.id_, s.key_, s.value_, p.name_, e.name_
+		from secrets_ s
+		inner join
+		environments_ e
+		on s.environment_id_ = e.id_
+		inner join
+		projects_ p
+		on p.id_ = e.project_id_
+		where p.name_ = $project
+		and e.name_ = $environment
+	`
+
+	mock.
+		ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs("my_cool_project", "staging").
+		WillReturnRows(
+			sqlmock.NewRows([]string{
+				"id_",
+				"key_",
+				"value_",
+				"project_name_",
+				"environment_name_",
+			}).
+				AddRow(1, "key_1", "value_1", "my_cool_project", "staging").
+				AddRow(2, "key_2", "value_2", "my_cool_project", "staging"),
+		)
+
+	err := cmd.Execute()
+
+	require.NoError(t, err)
+	require.Empty(t, errOut.String())
+
+	require.Equal(
+		t,
+		"key_1=value_1\nkey_2=value_2",
+		cmdOut.String(),
+	)
+
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func testSecretRemoveCmdHappyPath(
+	t *testing.T,
+	cmd *cobra.Command,
+	service secret.SecretService,
+	mock sqlmock.Sqlmock,
+) {
+	cmdIn := bytes.NewReader([]byte{})
+	cmdOut := bytes.NewBufferString("")
+	errOut := bytes.NewBufferString("")
+
+	cmdRemove := secret.NewCmdSecretRemove(
+		secret.NewHandlerSecretRemove(service),
+	)
+
+	cmd.AddCommand(cmdRemove)
+	cmd.SetArgs([]string{
+		"remove",
+		"-p",
+		"my_cool_project",
+		"-e",
+		"staging",
+		"key_1",
+	})
+	cmd.SetIn(cmdIn)
+	cmd.SetOut(cmdOut)
+	cmd.SetErr(errOut)
+
+	query := `
+		delete from secrets_
+		where id_ in (
+			select s.id_ from secrets_ s
+			inner join
+			environments_ e
+			on s.environment_id_ = e.id_
+			inner join
+			projects_ p
+			on e.project_id_ = p.id_
+			where p.name_ = $projectName
+			and e.name_ = $environmentName
+			and s.key_ = $key
+		)
+	`
+
+	mock.ExpectExec(regexp.QuoteMeta(query)).WithArgs(
+		"my_cool_project", "staging", "key_1",
+	).WillReturnResult(sqlmock.NewResult(23, 1))
+
+	err := cmd.Execute()
+
+	require.NoError(t, err)
+	require.Empty(t, errOut.String())
+
+	require.Equal(
+		t,
+		"", // maybe we want to execute a 'success' message or something in future
+		cmdOut.String(),
+	)
+
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
 // func testSecretListCmdDatabaseError(t *testing.T, mock sqlmock.Sqlmock, db *sql.DB) {
 // 	cmdIn := bytes.NewReader([]byte{})
 // 	cmdOut := bytes.NewBufferString("")
