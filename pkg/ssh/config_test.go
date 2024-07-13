d6acba47d098cdd2c761ced6fb7761977f2671f1
nixpig <143995476+nixpig@users.noreply.github.com>
2024-07-13 14:15:40 +0100

    test: add test for ssh config if host and identity already exist

--- pkg/ssh/config_test.go
+++ pkg/ssh/config_test.go
@@ -10,8 +10,9 @@ import (
 
 func TestSSHConfig(t *testing.T) {
 	scenarios := map[string]func(t *testing.T){
-		"test add identity to ssh config new host":      testAddIdentityToSSHConfigNewHost,
-		"test add identity to ssh config existing host": testAddIdentityToSSHConfigExistingHost,
+		"test add identity to ssh config new host":          testAddIdentityToSSHConfigNewHost,
+		"test add identity to ssh config existing host":     testAddIdentityToSSHConfigExistingHost,
+		"test add identity to ssh config existing identity": testAddIdentityToSSHConfigExistingIdentity,
 	}
 
 	for scenario, fn := range scenarios {
@@ -29,6 +30,7 @@ func testAddIdentityToSSHConfigNewHost(t *testing.T) {
 	id := "../../test/crypt_test_rsa"
 
 	err = ssh.AddIdentityToSSHConfig(id, f)
+	require.NoError(t, err)
 
 	// read contents of file and check
 	w, err := os.ReadFile(f.Name())
@@ -53,6 +55,31 @@ func testAddIdentityToSSHConfigExistingHost(t *testing.T) {
 	id := "../../test/crypt_test_rsa"
 
 	err = ssh.AddIdentityToSSHConfig(id, f)
+	require.NoError(t, err)
+
+	w, err := os.ReadFile(f.Name())
+	require.NoError(t, err)
+
+	require.Equal(
+		t,
+		"Host localhost\nAddKeysToAgent yes\nIgnoreUnknown UseKeychain\nUseKeychain yes\nIdentityFile ../../test/crypt_test_rsa\n",
+		string(w),
+	)
+}
+
+func testAddIdentityToSSHConfigExistingIdentity(t *testing.T) {
+	os.Setenv("APP_HOST", "localhost")
+	f, err := os.CreateTemp("", "tmp_ssh_config")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	f.WriteString("Host localhost\nAddKeysToAgent yes\nIgnoreUnknown UseKeychain\nUseKeychain yes\nIdentityFile ../../test/crypt_test_rsa\n")
+
+	id := "../../test/crypt_test_rsa"
+
+	err = ssh.AddIdentityToSSHConfig(id, f)
+	require.NoError(t, err)
 
 	w, err := os.ReadFile(f.Name())
 	require.NoError(t, err)
